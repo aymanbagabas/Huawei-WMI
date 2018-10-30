3961eb8ffabe29c97e808c4125bbb11ffcb595f8
Ayman Bagabas <ayman.bagabas@gmail.com>
2018-10-29 22:27:40 -0400

    micmute led works as expected and mbxp front speakers fix

--- drivers/platform/x86/huawei_wmi.c
+++ drivers/platform/x86/huawei_wmi.c
@@ -24,13 +24,11 @@
 #include <linux/input.h>
 #include <linux/input/sparse-keymap.h>
 #include <linux/acpi.h>
-#include <linux/leds.h>
-#include <linux/platform_device.h>
-#include <linux/device.h>
-#include <linux/miscdevice.h>
+#include <linux/wmi.h>
+#include <linux/huawei_wmi.h>
 
 MODULE_AUTHOR("Ayman Bagabas <ayman.bagabas@gmail.com>");
-MODULE_DESCRIPTION("Huawei WMI Hotkeys Driver");
+MODULE_DESCRIPTION("Huawei WMI hotkeys");
 MODULE_LICENSE("GPL");
 
 #define DEVICE_NAME "huawei"
@@ -50,14 +48,9 @@ MODULE_LICENSE("GPL");
 MODULE_ALIAS("wmi:"AMW0_GUID);
 MODULE_ALIAS("wmi:"EVENT_GUID);
 
-#define LED_MIC_PIN 0x04
-
-static const char * acpi_setcm[] = {
-    "\\_SB.PCI0.LPCB.EC0.WPIN",
-};
-
-static const char * acpi_getcm[] = {
-    "\\_SB.PCI0.LPCB.EC0.RPIN",
+enum {
+    MICMUTE_LED_ON = 0x00010B04,
+    MICMUTE_LED_OFF = 0x00000B04,
 };
 
 static const struct key_entry huawei_wmi_keymap[] __initconst = {
@@ -68,152 +61,29 @@ static const struct key_entry huawei_wmi_keymap[] __initconst = {
         { KE_IGNORE, 0x285, { KEY_VOLUMEDOWN } },
         { KE_IGNORE, 0x286, { KEY_VOLUMEUP } },
         { KE_KEY,    0x287, { KEY_MICMUTE } },
-        { KE_KEY,    0x289, { KEY_WLAN } },
+        { KE_KEY,    0x289, { KEY_RFKILL } },
         { KE_KEY,    0x28a, { KEY_PROG1 } },          // Huawei |M| button
         { KE_END,    0 }
 };
 
 struct huawei_wmi_device {
     struct input_dev *inputdev;
-    struct platform_device *pdev;
-    struct led_classdev micled;
 };
 static struct huawei_wmi_device *wmi_device;
 
-static struct attribute *huawei_attributes[] = {
-    NULL,
-};
-
-static const struct attribute_group huawei_attr_group = {
-    .attrs = huawei_attributes,
-};
-
-static int acpi_evalm(const char *method, struct acpi_object_list *arg_list, struct acpi_buffer *response)
+int huawei_wmi_micmute_led_set(bool on)
 {
+    u32 args = (on) ? MICMUTE_LED_ON: MICMUTE_LED_OFF;
+    struct acpi_buffer input = { (acpi_size)sizeof(args), &args };
     acpi_status status;
-    acpi_handle handle = ACPI_HANDLE(&wmi_device->pdev->dev);
 
-    status = acpi_evaluate_object(handle, (char *)method, arg_list, response);
-    if (ACPI_FAILURE(status)) {
-        pr_err("%s: Error evaluating method %s\n", MODULE_NAME, method);
-        return -EFAULT;
-    }
+    status = wmi_evaluate_method(AMW0_GUID, 0, 1, &input, NULL);
+    if (ACPI_FAILURE(status))
+        return status;
 
     return 0;
 }
-
-static int huawei_led_set(struct led_classdev *dev, enum led_brightness value)
-{
-    const char *method = acpi_setcm[0];
-    int err;
-
-    union acpi_object args[] = {
-        { .type = ACPI_TYPE_INTEGER, },
-        { .type = ACPI_TYPE_INTEGER, },
-        { .type = ACPI_TYPE_INTEGER, },
-    };
-    args[0].integer.value = 1;
-    args[1].integer.value = LED_MIC_PIN;
-    args[2].integer.value = (value > 0) ? LED_OFF: LED_ON;
-
-    struct acpi_object_list arg_list = {
-        .pointer = args,
-        .count = ARRAY_SIZE(args),
-    };
-    err = acpi_evalm(method, &arg_list, NULL);
-    if (err) {
-        pr_err("%s: Error in setting led brightness: %d\n", MODULE_NAME, err);
-        return -1;
-    }
-
-    return 0;
-}
-
-static enum led_brightness huawei_led_get(struct led_classdev *dev)
-{
-    const char * method = acpi_getcm[0];
-    struct acpi_buffer response = { ACPI_ALLOCATE_BUFFER, NULL };
-    union acpi_object *obj;
-    int result = 0;
-
-    union acpi_object args[] = {
-        { .type = ACPI_TYPE_INTEGER, },
-        { .type = ACPI_TYPE_INTEGER, },
-    };
-    args[0].integer.value = 1;
-    args[1].integer.value = LED_MIC_PIN;
-
-    struct acpi_object_list arg_list = {
-        .pointer = args,
-        .count = ARRAY_SIZE(args),
-    };
-
-    if (acpi_evalm(method, &arg_list, &response)) {
-        pr_err("%s: Error in getting led brightness\n", MODULE_NAME);
-    }
-
-    obj = (union acpi_object *)response.pointer;
-
-    if (obj && obj->type == ACPI_TYPE_INTEGER) {
-        result = obj->integer.value;
-        if (result == 0x55)
-            result = LED_ON;
-        else
-            result = LED_OFF;
-    }
-
-    kfree(response.pointer);
-    return result;
-}
-
-static int huawei_led_init(void)
-{
-    wmi_device->micled.name = "huawei::micmute";
-    wmi_device->micled.max_brightness = 1;
-    wmi_device->micled.brightness_set_blocking = huawei_led_set;
-    wmi_device->micled.brightness_get = huawei_led_get;
-
-    return led_classdev_register(&wmi_device->pdev->dev, &wmi_device->micled);
-}
-
-static void huawei_led_exit(void)
-{
-    if (!IS_ERR_OR_NULL(wmi_device->micled.dev))
-        led_classdev_unregister(&wmi_device->micled);
-}
-
-static int platform_init(void)
-{
-    int err;
-
-    wmi_device->pdev = platform_device_alloc(DEVICE_NAME, -1);
-    if (!wmi_device->pdev)
-        return -ENOMEM;
-    
-    platform_set_drvdata(wmi_device->pdev, wmi_device);
-
-    err = platform_device_add(wmi_device->pdev);
-    if (err)
-        goto err_pdev;
-
-    err = sysfs_create_group(&wmi_device->pdev->dev.kobj, &huawei_attr_group);
-    if (err)
-        goto err_sysfs;
-
-    return 0;
-
-err_sysfs:
-    platform_device_del(wmi_device->pdev);
-err_pdev:
-    platform_device_put(wmi_device->pdev);
-    return err;
-}
-
-static void platform_exit(void)
-{
-    sysfs_remove_group(&wmi_device->pdev->dev.kobj, &huawei_attr_group);
-    platform_device_unregister(wmi_device->pdev);
-}
+EXPORT_SYMBOL_GPL(huawei_wmi_micmute_led_set);
 
 static void huawei_wmi_process_key(struct input_dev *input_dev, int code)
 {
@@ -243,8 +113,13 @@ static void huawei_wmi_notify(u32 value, void *context)
 
     obj = (union acpi_object *)response.pointer;
 
-    if (obj && obj->type == ACPI_TYPE_INTEGER) {
+    if (!obj)
+        return;
+
+    if (obj->type == ACPI_TYPE_INTEGER) {
         huawei_wmi_process_key(wmi_device->inputdev, obj->integer.value);
+    } else {
+        pr_info("%s: Unknown response received %d\n", MODULE_NAME, obj->type);
     }
 
     kfree(response.pointer);
@@ -259,7 +134,7 @@ static int huawei_input_init(void)
     if (!wmi_device->inputdev)
         return -ENOMEM;
 
-    wmi_device->inputdev->name = "Huawei WMI Hotkeys Driver";
+    wmi_device->inputdev->name = "Huawei WMI hotkeys";
     wmi_device->inputdev->phys = "wmi/input0";
     wmi_device->inputdev->id.bustype = BUS_HOST;
 
@@ -298,7 +173,6 @@ static void huawei_input_exit(void)
 
 static int __init huawei_wmi_setup(void)
 {
-    acpi_status status;
     int err;
 
     wmi_device = kmalloc(sizeof(struct huawei_wmi_device), GFP_KERNEL);
@@ -309,20 +183,8 @@ static int __init huawei_wmi_setup(void)
     if (err)
         goto err_input;
 
-    err = platform_init();
-    if (err)
-        goto err_platform;
-
-    err = huawei_led_init();
-    if (err)
-        goto err_leds;
-
     return 0;
 
-err_leds:
-    platform_exit();
-err_platform:
-    huawei_input_exit();
 err_input:
     return err;
 }
@@ -330,8 +192,7 @@ err_input:
 static void huawei_wmi_destroy(void)
 {
     huawei_input_exit();
-    huawei_led_exit();
-    platform_exit();
+    kfree(wmi_device);
 }
 
 static int __init huawei_wmi_init(void)
@@ -355,7 +216,6 @@ static int __init huawei_wmi_init(void)
 static void __exit huawei_wmi_exit(void)
 {
     huawei_wmi_destroy();
-    kfree(wmi_device);
     pr_debug("%s: Driver unloaded successfully\n", MODULE_NAME);
 }
 
