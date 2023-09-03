6e56755a3a5f2ea488668d8e326f23c981ad0869
Ayman Bagabas <ayman.bagabas@gmail.com>
2023-09-03 15:54:38 -0400

    fix: compile on linux 6.2+

--- huawei-wmi.c
+++ huawei-wmi.c
@@ -470,7 +470,11 @@ static DEVICE_ATTR_RW(charge_control_start_threshold);
 static DEVICE_ATTR_RW(charge_control_end_threshold);
 static DEVICE_ATTR_RW(charge_control_thresholds);
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 2, 0)
+static int huawei_wmi_battery_add(struct power_supply *battery, struct acpi_battery_hook *hook)
+#else
 static int huawei_wmi_battery_add(struct power_supply *battery)
+#endif
 {
 	device_create_file(&battery->dev, &dev_attr_charge_control_start_threshold);
 	device_create_file(&battery->dev, &dev_attr_charge_control_end_threshold);
@@ -478,7 +482,11 @@ static int huawei_wmi_battery_add(struct power_supply *battery)
 	return 0;
 }
 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6, 2, 0)
+static int huawei_wmi_battery_remove(struct power_supply *battery, struct acpi_battery_hook *hook)
+#else
 static int huawei_wmi_battery_remove(struct power_supply *battery)
+#endif
 {
 	device_remove_file(&battery->dev, &dev_attr_charge_control_start_threshold);
 	device_remove_file(&battery->dev, &dev_attr_charge_control_end_threshold);
